d981ecf5c283773bd503c482f5e91c741a2bdb5e
iNakatsi <igorn4k@gmail.com>
2021-03-19 00:10:55 -0300

    7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,11 +68,13 @@
                                 </li>
                             @endif
 
+                            <!-- no register
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+                            -->
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
