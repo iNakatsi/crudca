12886ef840e3e6e4e7811896e90490d88fed4623
iNakatsi <igorn4k@gmail.com>
2021-03-23 01:45:02 -0300

    a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,13 +82,14 @@
                                 </li>
                             @endif
 
-                            <!-- no register -->
+                            <!-- no register
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
-
+                            -->
+                            
                         @else
                             <!-- Pagina para administradores -->
 
